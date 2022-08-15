ff62f7eadf74c3b1ca5e81bfd5822a1295df4e8d
Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
2022-08-15 15:47:09 +0300

    ASoC: SOF: ipc4-loader: Save the maximum number of libraries supported
    The firmware supports external libraries (containing modules) to be loaded
    runtime.
    The firmware configuration contains the maximum number of libraries
    supported, including the base firmware (which is library 0).
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>

--- sound/soc/sof/ipc4-loader.c
+++ sound/soc/sof/ipc4-loader.c
@@ -193,6 +193,11 @@ static int sof_ipc4_query_fw_configuration(struct snd_sof_dev *sdev)
 			trace_sof_ipc4_fw_config(sdev, "Trace log size", *tuple->value);
 			ipc4_data->mtrace_log_bytes = *tuple->value;
 			break;
+		case SOF_IPC4_FW_CFG_MAX_LIBS_COUNT:
+			trace_sof_ipc4_fw_config(sdev, "maximum number of libraries",
+						 *tuple->value);
+			ipc4_data->max_fw_libs = *tuple->value;
+			break;
 		default:
 			break;
 		}
