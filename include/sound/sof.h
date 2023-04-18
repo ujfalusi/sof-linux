8564b562a5e7b18912570eac3abddd553cad806b
Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
2023-04-18 11:16:33 +0300

    ASoC: SOF: Remove the string arrays used for default paths and firmware name
    The default_fw_path, default_tplg_path, default_lib_path and
    default_fw_filename are no longer used, they can be removed from
    sof_dev_desc.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>

--- include/sound/sof.h
+++ include/sound/sof.h
@@ -162,14 +162,6 @@ struct sof_dev_desc {
 	/* On demand DSP booting is possible on the platform */
 	bool on_demand_dsp_boot;
 
-	/* defaults paths for firmware, library and topology files */
-	const char *default_fw_path[SOF_IPC_TYPE_COUNT];
-	const char *default_lib_path[SOF_IPC_TYPE_COUNT];
-	const char *default_tplg_path[SOF_IPC_TYPE_COUNT];
-
-	/* default firmware name */
-	const char *default_fw_filename[SOF_IPC_TYPE_COUNT];
-
 	/* strings used for the firmware layout path/filename creation */
 	const char *vendor;
 	const char *platform;
