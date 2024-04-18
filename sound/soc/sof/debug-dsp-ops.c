ca2379907bb58aa78bbe7b20c5298094c4ea76ea
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2024-04-17 17:29:50 -0700

    ASoC: SOF: debug-dsp-ops: Add ops for trace init/free
    These will be used to debugging purposes when testing the different DSP
    ops.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Reviewed-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Liam Girdwood <liam.r.girdwood@intel.com>

--- sound/soc/sof/debug-dsp-ops.c
+++ sound/soc/sof/debug-dsp-ops.c
@@ -48,6 +48,18 @@ static int sof_dsp_ops_set_power_state(struct snd_sof_dev *sdev, char *state)
 	return 0;
 }
 
+static int sof_dsp_ops_trace_init(struct snd_sof_dev *sdev, bool init)
+{
+	if (init) {
+		sdev->fw_trace_is_supported = true;
+		return sof_fw_trace_init(sdev);
+	}
+
+	sof_fw_trace_free(sdev);
+	sdev->fw_trace_is_supported = false;
+	return 0;
+}
+
 static void sof_dsp_ops_unload_firmware(struct snd_sof_dev *sdev)
 {
 	/* release the currently loaded firmware */
@@ -201,6 +213,37 @@ static ssize_t sof_dsp_ops_tester_dfs_write(struct file *file, const char __user
 	struct snd_sof_dev *sdev = dfse->sdev;
 	size_t size;
 	char *string;
+	int ret;
+
+	if (!strcmp(dentry->d_name.name, "init_trace")) {
+		string = kzalloc(count + 1, GFP_KERNEL);
+		if (!string)
+			return -ENOMEM;
+
+		size = simple_write_to_buffer(string, count, ppos, buffer, count);
+		kfree(string);
+
+		ret = sof_dsp_ops_trace_init(sdev, true);
+		if (ret < 0)
+			return ret;
+
+		return size;
+	}
+
+	if (!strcmp(dentry->d_name.name, "free_trace")) {
+		string = kzalloc(count + 1, GFP_KERNEL);
+		if (!string)
+			return -ENOMEM;
+
+		size = simple_write_to_buffer(string, count, ppos, buffer, count);
+		kfree(string);
+
+		ret = sof_dsp_ops_trace_init(sdev, false);
+		if (ret < 0)
+			return ret;
+
+		return size;
+	}
 
 	if (!strcmp(dentry->d_name.name, "unload_fw")) {
 		string = kzalloc(count + 1, GFP_KERNEL);
@@ -330,5 +373,13 @@ int sof_dbg_dsp_ops_test_init(struct snd_sof_dev *sdev)
 	if (ret < 0)
 		return ret;
 
-	return sof_dsp_dsp_ops_create_dfse(sdev, "unload_fw", dsp_ops_debugfs, 0222);
+	ret = sof_dsp_dsp_ops_create_dfse(sdev, "unload_fw", dsp_ops_debugfs, 0222);
+	if (ret < 0)
+		return ret;
+
+	ret = sof_dsp_dsp_ops_create_dfse(sdev, "init_trace", dsp_ops_debugfs, 0222);
+	if (ret < 0)
+		return ret;
+
+	return sof_dsp_dsp_ops_create_dfse(sdev, "free_trace", dsp_ops_debugfs, 0222);
 }
