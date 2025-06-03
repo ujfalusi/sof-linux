f97e7f4397a8788a87dbbeee01eb0b2cc44ca69b
Bard Liao <yung-chuan.liao@linux.intel.com>
2025-06-03 16:33:14 +0800

    ASoC: rt711-sdca: wait codec init in hw_params
    Move regmap sync to rt711_sdca_update_status() when unattach_request
    is set, and only do regmap sync in resume when no reattach needed.
    And move waiting codec init to hw_params when the codec really need to
    be initialized. The change can shorten the resume time.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- sound/soc/codecs/rt711-sdca-sdw.c
+++ sound/soc/codecs/rt711-sdca-sdw.c
@@ -142,6 +142,7 @@ static int rt711_sdca_update_status(struct sdw_slave *slave,
 				enum sdw_slave_status status)
 {
 	struct rt711_sdca_priv *rt711 = dev_get_drvdata(&slave->dev);
+	int ret;
 
 	if (status == SDW_SLAVE_UNATTACHED)
 		rt711->hw_init = false;
@@ -169,7 +170,20 @@ static int rt711_sdca_update_status(struct sdw_slave *slave,
 		return 0;
 
 	/* perform I/O transfers required for Slave initialization */
-	return rt711_sdca_io_init(&slave->dev, slave);
+	ret = rt711_sdca_io_init(&slave->dev, slave);
+	if (ret < 0) {
+		dev_err(&slave->dev, "IO init failed: %d\n", ret);
+		return ret;
+	}
+
+	if (slave->unattach_request) {
+		regcache_cache_only(rt711->regmap, false);
+		regcache_sync(rt711->regmap);
+		regcache_cache_only(rt711->mbq_regmap, false);
+		regcache_sync(rt711->mbq_regmap);
+	}
+
+	return ret;
 }
 
 static int rt711_sdca_read_prop(struct sdw_slave *slave)
@@ -434,13 +448,10 @@ static int rt711_sdca_dev_system_suspend(struct device *dev)
 	return rt711_sdca_dev_suspend(dev);
 }
 
-#define RT711_PROBE_TIMEOUT 5000
-
 static int rt711_sdca_dev_resume(struct device *dev)
 {
 	struct sdw_slave *slave = dev_to_sdw_dev(dev);
 	struct rt711_sdca_priv *rt711 = dev_get_drvdata(dev);
-	unsigned long time;
 
 	if (!rt711->first_hw_init)
 		return 0;
@@ -456,14 +467,7 @@ static int rt711_sdca_dev_resume(struct device *dev)
 		goto regmap_sync;
 	}
 
-	time = wait_for_completion_timeout(&slave->initialization_complete,
-				msecs_to_jiffies(RT711_PROBE_TIMEOUT));
-	if (!time) {
-		dev_err(&slave->dev, "%s: Initialization not complete, timed out\n", __func__);
-		sdw_show_ping_status(slave->bus, true);
-
-		return -ETIMEDOUT;
-	}
+	return 0;
 
 regmap_sync:
 	slave->unattach_request = 0;
