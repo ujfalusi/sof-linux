6b549fc296715b13a9e407c1ec1491c44b00cbd5
Bard Liao <yung-chuan.liao@linux.intel.com>
2025-06-26 17:25:47 +0800

    fixup! soundwire: add sdw_slave_wait_for_initialization helper
    Fix the alignment issue.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- drivers/soundwire/slave.c
+++ drivers/soundwire/slave.c
@@ -109,7 +109,7 @@ int sdw_slave_wait_for_initialization(struct sdw_slave *slave, unsigned int time
 	unsigned long time;
 
 	time = wait_for_completion_timeout(&slave->initialization_complete,
-				msecs_to_jiffies(timeout));
+					   msecs_to_jiffies(timeout));
 	if (!time) {
 		dev_err(&slave->dev, "%s: Initialization not complete, timed out\n", __func__);
 		sdw_show_ping_status(slave->bus, true);
