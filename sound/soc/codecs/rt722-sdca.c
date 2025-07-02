8e123fc5af14cb286b53c4916357cf11bf7198f1
Bard Liao <yung-chuan.liao@linux.intel.com>
2025-07-02 15:17:02 +0800

    Revert "ASoC: rt722: wait codec init in hw_params"
    This reverts commit 8cec2a595c7cc91b2a7fc1d1378ef9e498178ca4.
    We should ensure the device is fully operational when it is resumed.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- sound/soc/codecs/rt722-sdca.c
+++ sound/soc/codecs/rt722-sdca.c
@@ -1114,8 +1114,6 @@ static void rt722_sdca_shutdown(struct snd_pcm_substream *substream,
 	snd_soc_dai_set_dma_data(dai, substream, NULL);
 }
 
-#define RT722_PROBE_TIMEOUT 5000
-
 static int rt722_sdca_pcm_hw_params(struct snd_pcm_substream *substream,
 				struct snd_pcm_hw_params *params,
 				struct snd_soc_dai *dai)
@@ -1169,10 +1167,6 @@ static int rt722_sdca_pcm_hw_params(struct snd_pcm_substream *substream,
 	num_channels = params_channels(params);
 	port_config.ch_mask = GENMASK(num_channels - 1, 0);
 	port_config.num = port;
-
-	retval = sdw_slave_wait_for_initialization(rt722->slave, RT722_PROBE_TIMEOUT);
-	if (retval < 0)
-		return retval;
 
 	retval = sdw_stream_add_slave(rt722->slave, &stream_config,
 					&port_config, 1, sdw_stream);
