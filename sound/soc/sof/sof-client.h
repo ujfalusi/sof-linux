9d339ec3d8004fa57362a9a34f9b663ae7603d07
Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
2026-07-24 15:09:17 +0300

    ASoC: SOF: sof-client: add sof_client_get_new_comp_id() accessor
    Add a client accessor that allocates a new unique component id from the
    DSP-global monotonic comp_id counter using atomic_fetch_inc(). Component
    ids belong to the DSP instance, not to an individual audio client, so a
    single allocator shared by all clients keeps the ids unique even when
    one DSP backs multiple audio cards. There is no matching free: ids are
    only handed out, never released.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>

--- sound/soc/sof/sof-client.h
+++ sound/soc/sof/sof-client.h
@@ -129,5 +129,6 @@ ssize_t sof_client_ipc4_find_debug_slot_offset_by_type(struct sof_client_dev *cd
 bool sof_client_is_dspless(struct sof_client_dev *cdev);
 int sof_client_get_num_cores(struct sof_client_dev *cdev);
 struct sof_ipc4_fw_data *sof_client_get_ipc4_fw_data(struct sof_client_dev *cdev);
+u32 sof_client_get_new_comp_id(struct sof_client_dev *cdev);
 
 #endif /* __SOC_SOF_CLIENT_H */
