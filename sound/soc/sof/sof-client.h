842d36c5784252700ab2a6c172cee302bc4b730f
Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
2026-07-27 15:36:39 +0300

    ASoC: SOF: sof-client: add function topology disable accessor
    Function topology loading is skipped when the user requested a specific
    topology file via the sof_tplg_filename module parameter, as that file
    is expected to be loaded as it is.
    
    Add sof_client_is_function_topology_disabled() so that audio code can
    query this without dereferencing struct snd_sof_dev.
    
    Note that this is separate from the topology.c local module parameter of
    the same name, which stays where it is.
    
    No functional change, the accessor has no users yet.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>

--- sound/soc/sof/sof-client.h
+++ sound/soc/sof/sof-client.h
@@ -57,6 +57,7 @@ size_t sof_client_get_ipc_max_payload_size(struct sof_client_dev *cdev);
 enum sof_ipc_type sof_client_get_ipc_type(struct sof_client_dev *cdev);
 const char *sof_client_get_topology_name(struct sof_client_dev *cdev);
 const char *sof_client_get_topology_prefix(struct sof_client_dev *cdev);
+bool sof_client_is_function_topology_disabled(struct sof_client_dev *cdev);
 bool sof_client_is_suspend_target_s0ix(struct sof_client_dev *cdev);
 
 /* DSP/firmware boot request */
